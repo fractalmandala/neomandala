d27321bf2bfa0244116f49d9dbb797f8fc580c4d
amritbrhat <i.amrit.p@gmail.com>
2023-05-29 18:22:57 +0530

    lots2

--- src/routes/code/+page.svelte
+++ src/routes/code/+page.svelte
@@ -1,36 +1,15 @@
 <script lang="ts">
-
-	import { onMount } from 'svelte'
-	import { snippets } from '$lib/utils/supabase'
+	import { onMount } from 'svelte';
 	import { breakZero, breakOne, breakTwo, themeMode, readingMode } from '$lib/stores/globalstores';
-
-		
-	let showsnippets:any
-
-
-	onMount(async() => {
-		showsnippets = await snippets();
-	});
 </script>
 
-<div class="rta-column"
+<div
+	class="rta-column"
 	class:levelzero={$breakZero}
 	class:levelone={$breakOne}
 	class:leveltwo={$breakTwo}
 	class:light={$themeMode}
 	class:dark={!$themeMode}
-	>
-	
-	<div class="rta-column">
-
-			{#if showsnippets && showsnippets.length > 0}
-				{#each showsnippets as item}
-					<h6 class="thin tt-u">
-						<a href="/note/{item.id}">
-							{item.title}
-						</a>
-					</h6>
-				{/each}
-			{/if}
-	</div>
+>
+	<div class="rta-column">wait</div>
 </div>

07584dfbc6cf15b2b96129c38842eb9f857d9b82
amritbrhat <i.amrit.p@gmail.com>
2023-06-09 20:45:52 +0530

    del15

--- src/lib/editor/buttonactions.ts
+++
@@ -1,7 +0,0 @@
-const chainer = (editor:any, fn:any) => editor.chain().focus()[fn]().run();
-
-const toggleBold = (editor: any) => chainer(editor, 'toggleBold');
-const toggleItalic = (editor: {chain: () => {(): any; new(): any; focus: {(): {(): any; new(): any; toggleItalic: {(): {(): any; new(): any; run: {(): any; new(): any;};}; new(): any;};}; new(): any;};};}) => editor.chain().focus().toggleItalic().run();
-const toggleStrike = (editor: {chain: () => {(): any; new(): any; focus: {(): {(): any; new(): any; toggleStrike: {(): {(): any; new(): any; run: {(): any; new(): any;};}; new(): any;};}; new(): any;};};}) => editor.chain().focus().toggleStrike().run();
-
-export { toggleBold, toggleItalic, toggleStrike };
